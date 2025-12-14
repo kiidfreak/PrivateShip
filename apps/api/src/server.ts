b0102f2537dbc68fe1d9242b6967e4b94db2879f
Immanuel Mwendia Maina <imaina671@gmail.com>
2025-12-15 01:46:49 +0300

    chore: enable loose cors for debugging

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -17,7 +17,7 @@ const server = Fastify({
 });
 
 server.register(cors, {
-    origin: ['http://localhost:3000'],
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 });
